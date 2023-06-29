da56c6a189c025fee0c1f4192a67012a6ffd347a
wysixontheflux <bitpoch@icloud.com>
2023-06-29 22:30:21 +0200

    Port manager

--- controller/server/PortManager.class.js
+++ controller/server/PortManager.class.js
@@ -1,24 +1,42 @@
+
+const allowedPorts = [
+    28960,
+    28961,
+    28962,
+    28963,
+    28964,
+    28965,
+    28966,
+    28967,
+    28968,
+    28969,
+    28970,
+    28971,
+    28972,
+    28973,
+    28974,
+    28975,
+];
+
 class PortManager {
-    constructor(startPort, endPort) {
-        this.availablePorts = Array.from({length: endPort - startPort + 1}, (_, i) => startPort + i);
-        this.usedPorts = [28965];
+    constructor() {
+        this.usedPorts = [];
     }
 
-    getAvailablePort() {
-        if (this.availablePorts.length === 0) {
-            throw new Error('No available ports');
+    getPort() {
+        let port = allowedPorts[Math.floor(Math.random() * allowedPorts.length)];
+        if (this.usedPorts.indexOf(port) === -1) {
+            this.usedPorts.push(port);
+            return port;
+        } else {
+            return this.getPort();
         }
-
-        const port = this.availablePorts.shift();
-        this.usedPorts.push(port);
-        return port;
     }
 
     releasePort(port) {
         const index = this.usedPorts.indexOf(port);
         if (index !== -1) {
             this.usedPorts.splice(index, 1);
-            this.availablePorts.push(port);
         }
     }
 }
