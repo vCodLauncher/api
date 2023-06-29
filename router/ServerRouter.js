0e9def481afc3d72a9806dacd713d937e49e3e41
wysixontheflux <bitpoch@icloud.com>
2023-06-29 23:06:08 +0200

    Port logic OK

--- router/ServerRouter.js
+++ router/ServerRouter.js
@@ -4,42 +4,45 @@ const pm = new PortManager();
 
 const router = Router();
 
-router.get('/server/all', (req, res) => {
+router.get('/all', (req, res) => {
 
 });
 
-router.get('/server/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res.json({ message: `Server ${req.params.id}` });
 });
 
-router.get('/server/start', (req, res) => {
+router.get('/start', (req, res) => {
     let port = 28960;
     let serverType = 'cod-1.5'
     let serverGamemode = 'sd'
     let serverMod = 'ro'
 })
 
-router.get('/attributePort', (req, res) => {
+router.post('/attributePort', (req, res) => {
     try {
-        const attributedPort = pm.attributePort();
+        const attributedPort = pm.attributePort(); // Utilisez pm directement
         res.json({ port: attributedPort });
     } catch (err) {
         res.status(500).send({ error: 'No available ports' });
     }
+
+    // http://localhost:3000/server/attributePort
 });
 
 router.get('/isPortInUse/:port', (req, res) => {
     const port = parseInt(req.params.port, 10);
     const inUse = pm.isPortInUse(port);
     res.json({ port, inUse });
+
+    // http://localhost:3000/server/isPortInuse/:?
 });
 
 router.delete('/releasePort/:port', (req, res) => {
     const port = parseInt(req.params.port, 10);
     pm.releasePort(port);
     res.json({ port, message: 'Port has been released' });
+
+    // http://localhost:3000/server/releasePort/:?
 });
-
-
-
 module.exports = router;
